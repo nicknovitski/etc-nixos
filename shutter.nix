427e5ae07cac503115d7a0e7e7eb0ce3fbda1ded
Nick Novitski <nicknovitski@gmail.com>
2016-11-06 03:26:53 +0000

    Extract shutter to nixos module and git submodule

--- shutter.nix
+++ shutter.nix
@@ -1,47 +1,22 @@
-{ stdenv, python3}:
+{ pkgs, ... }:
 
-let 
-  pythonEnv = python3.withPackages(ps: [ps.systemd]);
-in stdenv.mkDerivation {
-  name = "shutter";
-  propagatedBuildInputs = [pythonEnv];
-  passAsFile = ["script"];
-  script = ''
-    #!${pythonEnv}/bin/python3
-    
-    from systemd import login, journal
-    import time
-    import datetime
-    
-    def one_hour_ago():
-        return datetime.datetime.fromtimestamp(time.time() - 60**2)
-    
-    def journal():
-        j = journal.Reader()
-        j.this_boot()
-        j.add_match(SYSLOG_IDENTIFIER='systemd-logind')
-        return j
-    
-    def logouts():
-        j = journal()
-        return [x for x in j if (x['MESSAGE'].startswith('Removed session') and x['_SOURCE_REALTIME_TIMESTAMP'] > one_hour_ago())]
-    
-    def sessions():
-        return login.sessions()
-    
-    if (len(sessions()) > 0):
-        exit(0)
-    else:
-        if(len(logouts()) > 0):
-            exit(0)
-        else: 
-            exit(1)
-  '';
-  builder = builtins.toFile "builder.sh" "
-    source $stdenv/setup
+let
+  pkg = pkgs.callPackage ./occupado { };
+in
 
-    mkdir -p $out/bin
-    cp $scriptPath $out/bin/shutter
-    chmod a+x $out/bin/shutter
-  ";
+{
+  config = {
+    systemd.services.shutter = {
+      description = "Shut down the system when no-one's been logged in for an hour";
+      script = "${pkg}/bin/occupado";
+      serviceConfig.Type = "simple";
+      onFailure = [ "halt.target" ];
+    };
+
+    systemd.timers.shutter = {
+      description = "runs shutter service every hour, ten minutes before billing time";
+      timerConfig = { OnBootSec = "50minutes"; OnUnitActiveSec = "1hour"; };
+      wantedBy = [ "timers.target" ];
+    };
+  };
 }
